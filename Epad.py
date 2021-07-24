6231ad8692086539bc1193580d9028b9d06f8267
Bhaskar Mahor <51980844+mahorbhaskar@users.noreply.github.com>
2021-07-24 22:13:37 +0530

    Add files via upload

---
+++ Epad.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font, colorchooser, filedialog, messagebox
+import os
+
+main_application = tk.Tk()
+main_application.geometry('1200x800')
+main_application.title('Vpad text editor')
+main_application.wm_iconbitmap('icon.ico')
+
+
+############################################## main menu ###################################################
+# -------------------------------------&&&&&&&& End main menu &&&&&&&&&&& ----------------------------------
+main_menu = tk.Menu()
+#file icons
+new_icon = tk.PhotoImage(file='icons2/new.png')
+open_icon = tk.PhotoImage(file='icons2/open.png')
+save_icon = tk.PhotoImage(file='icons2/save.png')
+save_as_icon = tk.PhotoImage(file='icons2/save_as.png')
+exit_icon = tk.PhotoImage(file='icons2/exit.png')
+
+file = tk.Menu(main_menu, tearoff=False)
+
+
+
+#####edit
+#edit icons
+copy_icon = tk.PhotoImage(file='icons2/copy.png')
+paste_icon = tk.PhotoImage(file='icons2/paste.png')
+cut_icon = tk.PhotoImage(file='icons2/cut.png')
+clear_all_icon = tk.PhotoImage(file='icons2/clear_all.png')
+find_icon = tk.PhotoImage(file='icons2/find.png')
+
+edit = tk.Menu(main_menu, tearoff=False)
+
+
+######## view icons
+tool_bar_icon = tk.PhotoImage(file='icons2/tool_bar.png')
+status_bar_icon = tk.PhotoImage(file='icons2/status_bar.png')
+view = tk.Menu(main_menu, tearoff=False)
+
+
+######## color theme
+light_default_icon = tk.PhotoImage(file='icons2/light_default.png')
+light_plus_icon = tk.PhotoImage(file='icons2/light_plus.png')
+dark_icon = tk.PhotoImage(file='icons2/dark.png')
+red_icon = tk.PhotoImage(file='icons2/red.png')
+monokai_icon = tk.PhotoImage(file='icons2/monokai.png')
+night_blue_icon = tk.PhotoImage(file='icons2/night_blue.png')
+color_theme = tk.Menu(main_menu, tearoff=False)
+
+theme_choice = tk.StringVar()
+color_icons = (light_default_icon, light_plus_icon, dark_icon, red_icon, monokai_icon, night_blue_icon)
+
+color_dict = {
+    'Light Default ' : ('#000000', '#ffffff'),
+    'Light Plus' : ('#474747', '#e0e0e0'),
+    'Dark' : ('#c4c4c4', '#2d2d2d'),
+    'Red' : ('#2d2d2d', '#ffe8e8'),
+    'Monokai' : ('#d3b774', '#474747'),
+    'Night Blue' :('#ededed', '#6b9dc2')
+}
+# cascade
+main_menu.add_cascade(label='File', menu=file)
+main_menu.add_cascade(label='Edit', menu=edit)
+main_menu.add_cascade(label='View', menu=view)
+main_menu.add_cascade(label='Color Theme', menu=color_theme)
+
+############################################## toolbar  ###################################################
