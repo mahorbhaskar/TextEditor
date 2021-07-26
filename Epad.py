136b83899e17b3aed6669c83722deebaf577e068
Bhaskar Mahor <51980844+mahorbhaskar@users.noreply.github.com>
2021-07-26 21:56:09 +0530

    toolbar

--- Epad.py
+++ Epad.py
@@ -67,3 +67,92 @@ main_menu.add_cascade(label='View', menu=view)
 main_menu.add_cascade(label='Color Theme', menu=color_theme)
 
 ############################################## toolbar  ###################################################
+tool_bar = ttk.Label(main_application)
+tool_bar.pack(side=tk.TOP, fill=tk.X)
+
+## font box 
+font_tuple = tk.font.families()
+font_family = tk.StringVar()
+font_box = ttk.Combobox(tool_bar, width=30, textvariable=font_family, state='readonly')
+font_box['values'] = font_tuple
+font_box.current(font_tuple.index('Arial'))
+font_box.grid(row=0, column=0, padx=5)
+
+
+## size box 
+size_var = tk.IntVar()
+font_size = ttk.Combobox(tool_bar, width=14, textvariable = size_var, state='readonly')
+font_size['values'] = tuple(range(8,81))
+font_size.current(4)
+font_size.grid(row=0, column=1, padx=5)
+
+## bold button 
+bold_icon = tk.PhotoImage(file='icons2/bold.png')
+bold_btn = ttk.Button(tool_bar, image=bold_icon)
+bold_btn.grid(row=0, column=2, padx=5)
+
+## italic button 
+italic_icon = tk.PhotoImage(file='icons2/italic.png')
+italic_btn = ttk.Button(tool_bar, image=italic_icon)
+italic_btn.grid(row=0, column=3, padx=5)
+
+## underline button 
+underline_icon = tk.PhotoImage(file='icons2/underline.png')
+underline_btn = ttk.Button(tool_bar, image = underline_icon)
+underline_btn.grid(row = 0, column=4, padx=5)
+
+## font color button 
+font_color_icon = tk.PhotoImage(file='icons2/font_color.png')
+font_color_btn = ttk.Button(tool_bar, image=font_color_icon)
+font_color_btn.grid(row=0, column=5,padx=5)
+
+## align left 
+align_left_icon = tk.PhotoImage(file='icons2/align_left.png')
+align_left_btn = ttk.Button(tool_bar, image=align_left_icon)
+align_left_btn.grid(row=0, column=6, padx=5)
+
+## align center 
+align_center_icon = tk.PhotoImage(file='icons2/align_center.png')
+align_center_btn = ttk.Button(tool_bar, image=align_center_icon)
+align_center_btn.grid(row=0, column=7, padx=5)
+
+## align right 
+align_right_icon = tk.PhotoImage(file='icons2/align_right.png')
+align_right_btn = ttk.Button(tool_bar, image=align_right_icon)
+align_right_btn.grid(row=0, column=8, padx=5)
+
+# -------------------------------------&&&&&&&& End toolbar  &&&&&&&&&&& ----------------------------------
+
+############################################## text editor ###################################################
+
+text_editor = tk.Text(main_application)
+text_editor.config(wrap='word', relief=tk.FLAT)
+
+scroll_bar = tk.Scrollbar(main_application)
+text_editor.focus_set()
+scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
+text_editor.pack(fill=tk.BOTH, expand=True)
+scroll_bar.config(command=text_editor.yview)
+text_editor.config(yscrollcommand=scroll_bar.set)
+
+# font family and font size functionality 
+current_font_family = 'Arial'
+current_font_size = 12
+
+def change_font(event=None):
+    global current_font_family
+    current_font_family = font_family.get()
+    text_editor.configure(font=(current_font_family, current_font_size))
+
+def change_fontsize(event=None):
+    global current_font_size
+    current_font_size = size_var.get()
+    text_editor.configure(font=(current_font_family, current_font_size))
+
+
+font_box.bind("<<ComboboxSelected>>", change_font)
+font_size.bind("<<ComboboxSelected>>", change_fontsize)
+
+######## buttons functionality 
+
+# bold button functionality
