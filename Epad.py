0f7c06bf162fb7035d917e77c7e6c6a1a8c616fa
Bhaskar Mahor <51980844+mahorbhaskar@users.noreply.github.com>
2021-07-28 16:40:53 +0530

    MainMenu

--- Epad.py
+++ Epad.py
@@ -229,4 +229,236 @@ align_right_btn.configure(command=align_right)
 
 text_editor.configure(font=('Arial', 12))
 # -------------------------------------&&&&&&&& End text editor &&&&&&&&&&& ----------------------------------
+##############################################  status bar ###################################################
 
+status_bar = ttk.Label(main_application, text = 'Status Bar')
+status_bar.pack(side=tk.BOTTOM)
+
+text_changed = False 
+def changed(event=None):
+    global text_changed
+    if text_editor.edit_modified():
+        text_changed = True 
+        words = len(text_editor.get(1.0, 'end-1c').split())
+        characters = len(text_editor.get(1.0, 'end-1c'))
+        status_bar.config(text=f'Characters : {characters} Words : {words}')
+    text_editor.edit_modified(False)
+
+text_editor.bind('<<Modified>>', changed)
+
+
+# -------------------------------------&&&&&&&& End  status bar &&&&&&&&&&& ----------------------------------
+
+
+############################################## main menu functinality ###################################################
+
+## variable 
+url = ''
+
+## new functionality
+def new_file(event=None):
+    global url 
+    url = ''
+    text_editor.delete(1.0, tk.END)
+
+## file commands 
+file.add_command(label='New', image=new_icon, compound=tk.LEFT, accelerator='Ctrl+N', command=new_file)
+
+## open functionality
+
+def open_file(event=None):
+    global url 
+    url = filedialog.askopenfilename(initialdir=os.getcwd(), title='Select File', filetypes=(('Text File', '*.txt'), ('All files', '*.*')))
+    try:
+        with open(url, 'r') as fr:
+            text_editor.delete(1.0, tk.END)
+            text_editor.insert(1.0, fr.read())
+    except FileNotFoundError:
+        return 
+    except:
+        return 
+    main_application.title(os.path.basename(url))
+
+file.add_command(label='Open', image=open_icon, compound=tk.LEFT, accelerator='Ctrl+O', command=open_file)
+
+## save file 
+
+def save_file(event=None):
+    global url 
+    try:
+        if url:
+            content = str(text_editor.get(1.0, tk.END))
+            with open(url, 'w', encoding='utf-8') as fw:
+                fw.write(content)
+        else:
+            url = filedialog.asksaveasfile(mode = 'w', defaultextension='.txt', filetypes=(('Text File', '*.txt'), ('All files', '*.*')))
+            content2 = text_editor.get(1.0, tk.END)
+            url.write(content2)
+            url.close()
+    except:
+        return 
+
+file.add_command(label='Save', image=save_icon, compound=tk.LEFT, accelerator='Ctrl+S', command = save_file)
+
+
+## save as functionality 
+def save_as(event=None):
+    global url 
+    try:
+        content = text_editor.get(1.0, tk.END)
+        url = filedialog.asksaveasfile(mode = 'w', defaultextension='.txt', filetypes=(('Text File', '*.txt'), ('All files', '*.*')))
+        url.write(content)
+        url.close()
+    except:
+        return 
+
+
+file.add_command(label='Save As', image=new_icon, compound=tk.LEFT, accelerator='Ctrl+Alt+S', command=save_as)
+
+## exit functionality 
+
+def exit_func(event=None):
+    global url, text_changed
+    try:
+        if text_changed:
+            mbox = messagebox.askyesnocancel('Warning', 'Do you want to save the file ?')
+            if mbox is True:
+                if url:
+                    content = text_editor.get(1.0, tk.END)
+                    with open(url, 'w', encoding='utf-8') as fw:
+                        fw.write(content)
+                        main_application.destroy()
+                else:
+                    content2 = str(text_editor.get(1.0, tk.END))
+                    url = filedialog.asksaveasfile(mode = 'w', defaultextension='.txt', filetypes=(('Text File', '*.txt'), ('All files', '*.*')))
+                    url.write(content2)
+                    url.close()
+                    main_application.destroy()
+            elif mbox is False:
+                main_application.destroy()
+        else:
+            main_application.destroy()
+    except:
+        return 
+file.add_command(label='Exit', image=exit_icon, compound=tk.LEFT, accelerator='Ctrl+Q', command=exit_func)
+
+
+############ find functionality
+
+def find_func(event=None):
+
+    def find():
+        word = find_input.get()
+        text_editor.tag_remove('match', '1.0', tk.END)
+        matches = 0
+        if word:
+            start_pos = '1.0'
+            while True:
+                start_pos = text_editor.search(word, start_pos, stopindex=tk.END)
+                if not start_pos:
+                    break 
+                end_pos = f'{start_pos}+{len(word)}c'
+                text_editor.tag_add('match', start_pos, end_pos)
+                matches += 1
+                start_pos = end_pos
+                text_editor.tag_config('match', foreground='red', background='yellow')
+    
+    def replace():
+        word = find_input.get()
+        replace_text = replace_input.get()
+        content = text_editor.get(1.0, tk.END)
+        new_content = content.replace(word, replace_text)
+        text_editor.delete(1.0, tk.END)
+        text_editor.insert(1.0, new_content)
+
+    find_dialogue = tk.Toplevel()
+    find_dialogue.geometry('450x250+500+200')
+    find_dialogue.title('Find')
+    find_dialogue.resizable(0,0)
+
+    ## frame 
+    find_frame = ttk.LabelFrame(find_dialogue, text='Find/Replace')
+    find_frame.pack(pady=20)
+
+    ## labels
+    text_find_label = ttk.Label(find_frame, text='Find : ')
+    text_replace_label = ttk.Label(find_frame, text= 'Replace')
+
+    ## entry 
+    find_input = ttk.Entry(find_frame, width=30)
+    replace_input = ttk.Entry(find_frame, width=30)
+
+    ## button 
+    find_button = ttk.Button(find_frame, text='Find', command=find)
+    replace_button = ttk.Button(find_frame, text= 'Replace', command=replace)
+
+    ## label grid 
+    text_find_label.grid(row=0, column=0, padx=4, pady=4)
+    text_replace_label.grid(row=1, column=0, padx=4, pady=4)
+
+    ## entry grid 
+    find_input.grid(row=0, column=1, padx=4, pady=4)
+    replace_input.grid(row=1, column=1, padx=4, pady=4)
+
+    ## button grid 
+    find_button.grid(row=2, column=0, padx=8, pady=4)
+    replace_button.grid(row=2, column=1, padx=8, pady=4)
+
+    find_dialogue.mainloop()
+
+## edit commands 
+edit.add_command(label='Copy', image=copy_icon, compound=tk.LEFT, accelerator='Ctrl+C', command=lambda:text_editor.event_generate("<Control c>"))
+edit.add_command(label='Paste', image=paste_icon, compound=tk.LEFT, accelerator='Ctrl+V', command=lambda:text_editor.event_generate("<Control v>"))
+edit.add_command(label='Cut', image=cut_icon, compound=tk.LEFT, accelerator='Ctrl+X', command=lambda:text_editor.event_generate("<Control x>"))
+edit.add_command(label='Clear All', image=clear_all_icon, compound=tk.LEFT, accelerator='Ctrl+Alt+X', command= lambda:text_editor.delete(1.0, tk.END))
+edit.add_command(label='Find', image=find_icon, compound=tk.LEFT, accelerator='Ctrl+F', command = find_func)
+
+## view check button 
+
+show_statusbar = tk.BooleanVar()
+show_statusbar.set(True)
+show_toolbar = tk.BooleanVar()
+show_toolbar.set(True)
+
+def hide_toolbar():
+    global show_toolbar
+    if show_toolbar:
+        tool_bar.pack_forget()
+        show_toolbar = False 
+    else :
+        text_editor.pack_forget()
+        status_bar.pack_forget()
+        tool_bar.pack(side=tk.TOP, fill=tk.X)
+        text_editor.pack(fill=tk.BOTH, expand=True)
+        status_bar.pack(side=tk.BOTTOM)
+        show_toolbar = True 
+
+
+def hide_statusbar():
+    global show_statusbar
+    if show_statusbar:
+        status_bar.pack_forget()
+        show_statusbar = False 
+    else :
+        status_bar.pack(side=tk.BOTTOM)
+        show_statusbar = True 
+
+
+view.add_checkbutton(label='Tool Bar',onvalue=True, offvalue=0,variable = show_toolbar, image=tool_bar_icon, compound=tk.LEFT, command=hide_toolbar)
+view.add_checkbutton(label='Status Bar',onvalue=1, offvalue=False,variable = show_statusbar, image=status_bar_icon, compound=tk.LEFT, command=hide_statusbar)
+
+
+## color theme 
+def change_theme():
+    chosen_theme = theme_choice.get()
+    color_tuple = color_dict.get(chosen_theme)
+    fg_color, bg_color = color_tuple[0], color_tuple[1]
+    text_editor.config(background=bg_color, fg=fg_color) 
+count = 0 
+for i in color_dict:
+    color_theme.add_radiobutton(label = i, image=color_icons[count], variable=theme_choice, compound=tk.LEFT, command=change_theme)
+    count += 1 
+
+
+
+# -------------------------------------&&&&&&&& End main menu  functinality&&&&&&&&&&& ----------------------------------
